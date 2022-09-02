e17dae03f820177ab659a200c5b0244b332b5901
Lalit <lalit.c@thinkbridge.in>
2022-09-02 14:36:15 +0530

    updated

--- restartdoc.sh
+++ restartdoc.sh
@@ -21,9 +21,6 @@ if [ "$contnm" = "$var" ]; then
         #ssh -i $key $usrnm@$ip "sudo docker restart $contnm"
 else
         echo "$contnm container not found"
-<<<<<<< HEAD
-fi
-=======
 fi
 
->>>>>>> main
+
