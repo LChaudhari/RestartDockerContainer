6c9065f723730c986f29d366a7ac66bbfc1ab50b
LChaudhari <105642006+LChaudhari@users.noreply.github.com>
2022-09-06 11:27:22 +0530

    Update restartdoc.sh

--- restartdoc.sh
+++ restartdoc.sh
@@ -14,10 +14,10 @@ echo "Username: $usrnm";
 echo "IPAddress: $ip";
 echo "ContainerName: $contnm";
 
-var=$(ssh $usrnm@$ip "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
+var=$(ssh -T $usrnm@$ip "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
 
 if [ "$contnm" = "$var" ]; then
-        ssh $usrnm@$ip "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
+        ssh -T $usrnm@$ip "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
         #ssh -i $key $usrnm@$ip "sudo docker restart $contnm"
 else
         echo "$contnm container not found"
