31d3471d7932a2d1dcb7297a425eb4f6a5376958
Lalit <lalit.c@thinkbridge.in>
2022-09-08 13:54:00 +0530

    Modified restart.sh

--- restartdoc.sh
+++ restartdoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts k:u:p:c: flag
+while getopts u:p:c: flag
 do
     case "${flag}" in
         # k) key=${OPTARG};;
@@ -14,13 +14,11 @@ echo "Username: $usrnm";
 echo "IPAddress: $ip";
 echo "ContainerName: $contnm";
 
-var=$(ssh -i $key $usrnm@$ip  "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
+var=$(ssh -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
 
 if [ "$contnm" = "$var" ]; then
-        ssh -i $key $usrnm@$ip  "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
+        ssh -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
         #ssh -i $key $usrnm@$ip "sudo docker restart $contnm"
 else
         echo "$contnm container not found"
 fi
-
-
