61546fb2f30d61d2f420a0ab16818017196c2976
LChaudhari <105642006+LChaudhari@users.noreply.github.com>
2022-09-08 13:42:25 +0530

    Update restartdoc.sh

--- restartdoc.sh
+++ restartdoc.sh
@@ -14,10 +14,10 @@ echo "Username: $usrnm";
 echo "IPAddress: $ip";
 echo "ContainerName: $contnm";
 
-var=$(ssh -tt -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
+var=$(ssh remotehost 'bash -s' -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.Names}}' | grep "$contnm"; exit 0")
 
 if [ "$contnm" = "$var" ]; then
-        ssh -tt -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
+        ssh -tt remotehost 'bash -s' "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
         #ssh -i $key $usrnm@$ip "sudo docker restart $contnm"
 else
         echo "$contnm container not found"
