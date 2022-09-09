0f0a52bde49b03e5a4e7cf6e8508872c96b3c69b
LChaudhari <105642006+LChaudhari@users.noreply.github.com>
2022-09-09 14:56:49 +0530

    Update restartdoc.sh

--- restartdoc.sh
+++ restartdoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-slack_notification(){
+slack_notification (){
    SLACK_WEBHOOK_URL= ${{ secrets.SLACK_GITHUB_ACTIONS_ALERTS_WEBHOOK }}
    notify_string=$1
    curl -sk -d "payload={\"channel\": \"#low-high-priority\", \"username\": \"Docker Container Restart Successful\", \"text\":  \"${notify_string}\"}" ${SLACK_WEBHOOK_URL}
@@ -25,7 +25,7 @@ var=$(ssh -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --forma
 if [ "$contnm" = "$var" ]; then
         ssh -o "StrictHostKeyChecking no" $usrnm@$ip -t "sudo docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}'; sudo docker restart $contnm"
         echo "Container Restart Successfully"
-        slack_notification ()
+        slack_notification;;
 else
         echo "$contnm container not found"
 fi
