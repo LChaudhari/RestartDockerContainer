f682f214693fc01b5cde4908b723859338a8fdeb
LChaudhari <105642006+LChaudhari@users.noreply.github.com>
2022-09-28 12:01:44 +0530

    Update qbotrestart.sh

--- qbotrestart.sh
+++ qbotrestart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+list=[172.31.96.27,172.31.105.164]
+for ips in list:
+ ssh -o "StrictHostKeyChecking no" ec2-user@$ips -t 'sudo docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Image}}"; sudo docker restart $(sudo docker ps -a -q)'
+ echo "Container Restart Successfully"
  
-ssh -o "StrictHostKeyChecking no" ec2-user@172.31.96.27 -t 'sudo docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Image}}"; sudo docker restart $(sudo docker ps -a -q)'
-echo "Container Restart Successfully"
